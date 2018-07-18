34f942c924b9b0005d10a8c4ed345227feb9012a
Daniela Lemow <danielalemow@danielas-macbook-pro.local>
2018-07-18 20:14:49 +1200

    Added stored properties and a default initialiser to the fraction class.

--- prog2.1/prog2.1/Fraction.swift
+++ prog2.1/prog2.1/Fraction.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+/**
+ * Represents a number as a fraction of two integers.
+ */
+
+class Fraction {
+    
+    // STORED PROPERTIES
+    
+    var num: Int // Numerator
+    var dem: Int // Denominator
+    
+    // INITIALISERS
+    
+    /**
+     * Default initialiser
+     * Numerator gets set to 0 and denominator gets set to 1
+     */
+    
+    init() {
+        // Setting numerator to 0 and
+        // denominator to 1 is equivalent
+        // to setting the fraction to zero
+        self.num = 0
+        self.dem = 1
+    }
+}

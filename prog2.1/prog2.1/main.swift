18b9fa18accf13b05f88d0b4af347cabe3423dbd
Daniela Lemow <danielalemow@danielas-macbook-pro.local>
2018-07-18 21:06:54 +1200

    Updated test code in the main file.

--- prog2.1/prog2.1/main.swift
+++ prog2.1/prog2.1/main.swift
@@ -17,3 +17,16 @@ u = Fraction.init()
 // Show the state of the object's internal variables
 print("u = \(u.num)/\(u.dem)")
 
+// Change the values of object's internal variables
+u.num = 2
+u.dem = 3
+
+// Show the state of the object's internal variables
+print("u = \(u.num)/\(u.dem)")
+
+// Define and create a new instance of a Fraction object
+var x: Fraction = Fraction()
+x.num = -5
+x.dem = 1
+
+print("x = \(x.num)/\(x.dem)")
